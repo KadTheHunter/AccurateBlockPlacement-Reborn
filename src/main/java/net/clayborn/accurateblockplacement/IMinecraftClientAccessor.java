eefa252ba34cd0165c74045a4ebf4d10bec9442c
chtko <133684869+chtko@users.noreply.github.com>
2025-05-11 20:37:40 +0600

    UPD ru_ru.json

--- src/main/java/net/clayborn/accurateblockplacement/IMinecraftClientAccessor.java
+++
@@ -1,8 +0,0 @@
-package net.clayborn.accurateblockplacement;
-
-public interface IMinecraftClientAccessor
-{
-	void accurateblockplacement_DoItemUseBypassDisable();
-	void accurateblockplacement_SetItemUseCooldown(int cooldown);
-	public int accurateblockplacement_GetItemUseCooldown();
-}
